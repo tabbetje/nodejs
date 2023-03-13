49a7acbcf63ee4ff92b42e284235673cb429cc9f
Bert Otte <bert@otte.nu>
2023-03-13 21:46:40 +0100

    API put, post, get, delete

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mysql = require("mysql");
+const SqlToJson = require("sql-to-json");
+const runAsyncGen = require("run-async-gen");
+const fs = require("fs");
 
 // Create connection
 const db = mysql.createConnection({
@@ -18,17 +21,91 @@ db.connect((err) => {
     console.log("MySql Connected");
  });
 
- const app = express();
+// runAsyncGen(GenerateJsonFile(), function(err) {
+//   if (err) {
+//       console.log(err);
+//       process.exit(1);
+//   }
+//   console.log('Done');
+// });
 
- // Create data object
+function* GenerateJsonFile() {
+	// const mySqlDbClient = mysql.createConnection({credentials object});
+	// mySqlDbClient.connect();
+
+	const struct = {
+    "type": "object",
+    "preLoadTables": {
+        "direct_publishers": "select * from publishers",
+        "cost_models": "select * from costModels",
+        "cost_models_countries": "select * from cm_countries",
+        "cost_model_types": "select * from cm_types",
+        "activities": "select * from activities",
+        "countries": "select * from countries"
+    },
+    "memQuery": "select id from direct_publishers",
+    "keyField": "id",
+    "refField": "id",
+    "fields": [{
+        "type": "object",
+        "memQuery": "select dp.name, ac.name as activity_name from direct_publishers dp inner join activities ac on dp.activity_id = ac.id where dp.id = ?",
+        "fields": [{
+            "dbName": "name",
+            "name": "name",
+            "type": "string"
+        }, {
+            "dbName": "activity_name",
+            "name": "activity",
+            "type": "string"
+        }, {
+            "name": "costModels",
+            "type": "object",
+            "nullable": true,
+            "memQuery": "select id, domain from cost_models where publisher_id = ?",
+            "keyField": "domain",
+            "refField": "id",
+            "fields": [{
+                "type": "object",
+                "memQuery": "select c.country_code from cost_models cm inner join cost_models_countries cmc on cm.id = cmc.cost_model_id inner join countries c on cmc.country_id = c.id where cm.id = ?",
+                "keyField": "country_code",
+                "fields": [{
+                    "type": "object",
+                    "memQuery": [
+                        "select c.country_code from cost_models cm inner join cost_models_countries cmc on cm.id = cmc.cost_model_id inner join countries c on cmc.country_id = c.id where cm.id = ?",
+                        "select 'all' as country_code"
+                    ],
+                    "fields": [{
+                        "dbName": "type_name",
+                        "name": "type",
+                        "type": "string"
+                    }, {
+                        "dbName": "value",
+                        "name": "value",
+                        "type": "number"
+                    }]
+                }]
+            }]
+        }]
+    }]
+};
+	const instance = new SqlToJson(db);
+	const dataAsJson = yield* instance.executeGen(struct);
+	
+	fs.writeFileSync('output.json', JSON.stringify(dataAsJson));
+	db.end();
+}
+
+const app = express();
+
+// Create data object
 app.get('/test', function(request, response){
-  db.query('select * from employee', function(error, results){
-      if ( error ){
-          response.status(400).send('Error in database operation');
-      } else {
-          response.send(results);
-      }
-  });
+  // db.query('select * from employee', function(error, results){
+  //     if ( error ){
+  //         response.status(400).send('Error in database operation');
+  //     } else {
+  //         response.send(results);
+  //     }
+  // });
 });
 
 // App listen to port 3000
